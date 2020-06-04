c09d54bf00732f3403bb4912e2e856f5214676ef
Antonis Christofides <antonis@antonischristofides.com>
2020-06-04 16:46:23 +0300

    Automatically recreate container if image has changed

--- run.sh
+++ run.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
-if docker ps -a | grep -q ' enhydris-dev$'; then
-    echo "Container already exists; starting it..."
-    docker start -i enhydris-dev
-else
-    echo "Container doesn't exist; creating it"
+
+docker_image_has_changed() {
+    image_id=`docker image ls | grep '^enhydris-dev'|awk '{ print $3 }'`
+    container_image_id=`docker ps -a | grep ' enhydris-dev$' | awk '{ print $2 }'`
+    [[ -z "$image_id"
+       || ( "$image_id" != "$container_image_id"
+            &&
+           "$container_image_id" != "enhydris-dev"
+          )
+    ]]
+    return $?
+}
+
+if docker_image_has_changed; then
+    echo "Container doesn't exist or the image has changed; creating new container"
+    docker rm enhydris-dev
     docker run --name enhydris-dev -it \
         -v `pwd`/enhydris:/opt/enhydris \
         -v `pwd`/enhydris-openhigis:/opt/enhydris-openhigis \
         -v `pwd`/enhydris-synoptic:/opt/enhydris-synoptic \
         -v `pwd`/enhydris-autoprocess:/opt/enhydris-autoprocess \
         -v `pwd`/dbdump:/dbdump -p 8001:8000 enhydris-dev
+else
+    echo "Container already exists; starting it..."
+    docker start -i enhydris-dev
 fi
