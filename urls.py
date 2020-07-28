0c4fd3386e07da601b793269fa99801e2f764dc5
Antonis Christofides <antonis@antonischristofides.com>
2020-07-28 16:06:30 +0300

    Upgrade to isort v5 (fixes #8) (#9)
    isort>=5 does not support the --recursive option (it works recursively
    anyway), so it had to be removed. In addition, apparently it searches
    from the current directory upwards for .isort.cfg, and not from the
    checked file upwards, so we need to change directory in order to check
    different working directories.

--- urls.py
+++ urls.py
@@ -2,7 +2,8 @@ from django.conf.urls.static import static
 from django.urls import include, path
 
 from enhydris import urls as enhydris_urls
-from enhydris_openhigis import urls as enhydris_openhigis_urls
+
+from enhydris_openhigis import urls as enhydris_openhigis_urls  # isort:skip
 
 urlpatterns = [
     path("openhigis/", include(enhydris_openhigis_urls)),
