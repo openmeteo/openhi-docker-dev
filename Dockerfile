94673211d16c0b35ce21a4b894a5c324d6ffffe4
Antonis Christofides <antonis@antonischristofides.com>
2020-12-16 17:00:13 +0200

    Gracefully stop VNC server
    If the VNC server isn't stopped gracefully, it leaves some files in /tmp
    which make it impossible to start again.

--- Dockerfile
+++ Dockerfile
@@ -28,7 +28,7 @@ RUN apt-get -y update && apt-get install -y --no-install-recommends \
         python3-dev libjpeg-dev libfreetype6-dev \
         tmux apache2 google-chrome-stable cgi-mapserver \
         tightvncserver lxde xfonts-base xfonts-75dpi xfonts-100dpi \
-        npm gettext \
+        npm gettext psmisc \
     && apt-get clean
 
 # Replace Debian's too old npm with a newer one
--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 service apache2 start
 service postgresql start
 service rabbitmq-server start
-( echo "Starting VNC server ..."; USER=root vncserver :1 -geometry 1280x800 -depth 24 ) &
+( echo "Starting VNC server..."; USER=root vncserver :1 -geometry 1280x800 -depth 24 ) &
 sleep 2
 cd /opt/enhydris
 echo "Container is running"
@@ -15,6 +15,7 @@ echo " - 'runsynoptic' to create the synoptic static files"
 echo " - 'celery worker -A enhydris -l info --concurrency=1' to run the celery worker"
 echo " - 'exit' to stop the container"
 bash
+echo "Killing VNC server..."; killall Xtightvnc
 service rabbitmq-server stop
 service postgresql stop
 service apache2 stop
