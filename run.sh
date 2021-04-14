60e43819a9f5c2d98298dc3d3734bd71ca6853ee
Antonis Christofides <antonis@antonischristofides.com>
2021-04-14 12:07:34 +0300

    Make PostgreSQL accessible from host

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ if docker_image_has_changed; then
         -v `pwd`/enhydris-openhigis:/opt/enhydris-openhigis \
         -v `pwd`/enhydris-synoptic:/opt/enhydris-synoptic \
         -v `pwd`/enhydris-autoprocess:/opt/enhydris-autoprocess \
-        -v `pwd`/shared:/shared -p 8001:80 -p 5901:5901 enhydris-dev
+        -v `pwd`/shared:/shared -p 15432:5432 -p 8001:80 -p 5901:5901 enhydris-dev
 else
     echo "Container already exists; starting it..."
     docker start -i enhydris-dev
